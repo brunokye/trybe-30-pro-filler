30f6997b80f9a741de70c5e21c621b05f9ee991f
Bruno Ferreira <contatobrunokye@gmail.com>
2023-07-05 15:41:27 -0300

    feat: requisito 03

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,39 @@
-from pro_filer.actions.main_actions import show_preview  # NOQA
+from pro_filer.actions.main_actions import show_preview
+
+
+def test_show_preview_empty_context(capsys):
+    context = {
+        "all_files": [],
+        "all_dirs": []
+    }
+
+    show_preview(context)
+    captured = capsys.readouterr()
+
+    assert captured.out == (
+        f'Found {len(context["all_files"])} files '
+        f'and {len(context["all_dirs"])} directories\n'
+    )
+
+
+def test_show_preview_normal_context(capsys):
+    context = {
+        "all_files": [
+            'file1.txt', 'file2.txt', 'file3.txt',
+            'file4.txt', 'file5.txt', 'file6.txt'
+        ],
+        "all_dirs": [
+            'dir1', 'dir2', 'dir3',
+            'dir4', 'dir5', 'dir6'
+        ]
+    }
+
+    show_preview(context)
+    captured = capsys.readouterr()
+
+    assert captured.out == (
+        f'Found {len(context["all_files"])} files '
+        f'and {len(context["all_dirs"])} directories\n'
+        f'First 5 files: {context["all_files"][:5]}\n'
+        f'First 5 directories: {context["all_dirs"][:5]}\n'
+    )
