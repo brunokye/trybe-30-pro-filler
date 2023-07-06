c06262cda8626716b6b8a37832ad7cd755d2c546
Bruno Ferreira <contatobrunokye@gmail.com>
2023-07-06 18:07:03 -0300

    feat: requisito 05

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,22 @@
-from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+from pro_filer.actions.main_actions import show_disk_usage
+from pro_filer.cli_helpers import _get_printable_file_path
+
+
+def test_show_disk_usage_normal_paths(tmp_path, capsys):
+    fake_file_01 = tmp_path / 'fake_01.json'
+    fake_file_01.touch()
+    fake_file_01.write_text('a')
+
+    fake_file_02 = tmp_path / 'fake_02.json'
+    fake_file_02.touch()
+
+    context = {'all_files': [str(fake_file_01), str(fake_file_02)]}
+
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+
+    assert captured.out == (
+        f"'{_get_printable_file_path(str(fake_file_01))}':        1 (100%)\n"
+        f"'{_get_printable_file_path(str(fake_file_02))}':        0 (0%)\n"
+        'Total size: 1\n'
+    )
